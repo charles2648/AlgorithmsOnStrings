e1e987649a81a131b39a9f82dd7670587a2cbe84
androidwargames <adwagner89@gmail.com>
2016-07-25 22:01:20 -0500

    finished Trie assignments

--- PA1/trie.py
+++ PA1/trie.py
@@ -9,15 +9,59 @@ import sys
 # contains all the trie edges outgoing from the corresponding
 # node, and the keys are the letters on those edges, and the
 # values are the node IDs to which these edges lead.
+
+
 def build_trie(patterns):
-    tree = dict()
-    # write your code here
-    return tree
+    t = Trie()
+    for p in patterns:
+        cn = t.root
+        for i in range(len(p)):
+            cs = p[i]
+            strings = [s.s for s in cn.outs]
+            if cs in strings:
+                e = strings.index(cs)
+                cn = cn.outs[e].tail
+            else:
+                cn = t.addNode(cn, cs)
+    return t
+
+
+class Trie:
+    def __init__(self):
+        self.root = self.Node(0)
+        self.count = 1
+        self.nodes = []
+        self.edges = []
+
+    class Node:
+        def __init__(self, i):
+            self.ins = []
+            self.outs = []
+            self.index = i
+
+    class Edge:
+        def __init__(self, s, n, i):
+            self.s = s
+            self.head = n
+            self.tail = Trie.Node(i)
+            self.tail.ins.append(self)
+
+        def __str__(self):
+            return self.s
+
+        __repr__ = __str__
+
+    def addNode(self, n, s):
+        e = Trie.Edge(s, n, self.count)
+        self.edges.append(e)
+        self.count += 1
+        n.outs.append(e)
+        self.nodes.append(e.tail)
+        return e.tail
 
 
 if __name__ == '__main__':
     patterns = sys.stdin.read().split()[1:]
     tree = build_trie(patterns)
-    for node in tree:
-        for c in tree[node]:
-            print("{}->{}:{}".format(node, tree[node][c], c))
+    for edge in tree.edges:
+        print("{}->{}:{}".format(edge.head.index, edge.tail.index, edge.s))
--- PA1/trie_matching.py
+++ PA1/trie_matching.py
@@ -3,23 +3,88 @@ import sys
 
 NA = -1
 
-class Node:
-	def __init__ (self):
-		self.next = [NA] * 4
 
-def solve (text, n, patterns):
-	result = []
+def solve(text, patterns):
+    t = Trie()
+    for p in patterns:
+        cn = t.root
+        for i in range(len(p)):
+            cs = p[i]
+            strings = [s.s for s in cn.outs]
+            if cs in strings:
+                e = strings.index(cs)
+                cn = cn.outs[e].tail
+            else:
+                cn = t.addNode(cn, cs)
+    return t.trieMatch(text)
 
-	// write your code here
 
-	return result
+class Trie:
+    def __init__(self):
+        self.root = self.Node(0)
+        self.count = 1
+        self.nodes = []
+        self.edges = []
 
-text = sys.stdin.readline ().strip ()
-n = int (sys.stdin.readline ().strip ())
+    class Node:
+        def __init__(self, i):
+            self.ins = []
+            self.outs = []
+            self.index = i
+
+    class Edge:
+        def __init__(self, s, n, i):
+            self.s = s
+            self.head = n
+            self.tail = Trie.Node(i)
+            self.tail.ins.append(self)
+
+        def __str__(self):
+            return self.s
+
+        __repr__ = __str__
+
+    def addNode(self, n, s):
+        e = Trie.Edge(s, n, self.count)
+        self.edges.append(e)
+        self.count += 1
+        n.outs.append(e)
+        self.nodes.append(e.tail)
+        return e.tail
+
+    def prefixMatch(self, text):
+        v = self.root
+        i = 0
+        while True:
+            # check if we're done
+            if len(v.outs) == 0:
+                return True
+            # if not, get next letter, see if it's an edge away
+            strings = [s.s for s in v.outs]
+            try:
+                symbol = text[i]
+            except IndexError:
+                return False
+            if symbol in strings and i < len(text):
+                n = strings.index(symbol)
+                v = v.outs[n].tail
+                i += 1
+            else:
+                return False
+
+    def trieMatch(self, text):
+        matches = []
+        for i in range(len(text)):
+            if self.prefixMatch(text[i:]):
+                matches.append(i)
+        return matches
+
+text = sys.stdin.readline().strip()
+n = int(sys.stdin.readline().strip())
 patterns = []
-for i in range (n):
-	patterns += [sys.stdin.readline ().strip ()]
+for i in range(n):
+    patterns += [sys.stdin.readline().strip()]
 
-ans = solve (text, n, patterns)
+ans = solve(text, patterns)
 
-sys.stdout.write (' '.join (map (str, ans)) + '\n')
+sys.stdout.write(' '.join(map(str, ans)) + '\n')
