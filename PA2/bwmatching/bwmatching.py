5c1d9b6d38151f5c1c119f7f95bf681298a56332
androidwargames <adwagner89@gmail.com>
2016-08-07 21:59:11 -0500

    finished bwtinverse.py

--- PA2/bwmatching/bwmatching.py
+++ PA2/bwmatching/bwmatching.py
@@ -6,8 +6,8 @@ def PreprocessBWT(bwt):
   """
   Preprocess the Burrows-Wheeler Transform bwt of some text
   and compute as a result:
-    * starts - for each character C in bwt, starts[C] is the first position 
-        of this character in the sorted array of 
+    * starts - for each character C in bwt, starts[C] is the first position
+        of this character in the sorted array of
         all characters of the text.
     * occ_count_before - for each character C in bwt and each position P in bwt,
         occ_count_before[C][P] is the number of occurrences of character C in bwt
@@ -18,26 +18,36 @@ def PreprocessBWT(bwt):
 
 
 def CountOccurrences(pattern, bwt, starts, occ_counts_before):
-  """
-  Compute the number of occurrences of string pattern in the text
-  given only Burrows-Wheeler Transform bwt of the text and additional
-  information we get from the preprocessing stage - starts and occ_counts_before.
-  """
-  # Implement this function yourself
-  return 0
-     
+    """
+    Compute the number of occurrences of string pattern in the text
+    given only Burrows-Wheeler Transform bwt of the text and additional
+    information we get from the preprocessing stage - starts and occ_counts_before.
+    """
+    order = [0,0,0,0,0]
+    for i in range(len(sorted)):
+        x = select(sorted[i])
+        if order[x] == 0:
+            order[x] = i
+    # Implement this function yourself
+    return 0
+
+
+def select(inp):
+    a = ['$','A','C','G','T']
+    return a.index(inp)
+
 
 
 if __name__ == '__main__':
-  bwt = sys.stdin.readline().strip()
-  pattern_count = int(sys.stdin.readline().strip())
-  patterns = sys.stdin.readline().strip().split()
-  # Preprocess the BWT once to get starts and occ_count_before.
-  # For each pattern, we will then use these precomputed values and
-  # spend only O(|pattern|) to find all occurrences of the pattern
-  # in the text instead of O(|pattern| + |text|).  
-  starts, occ_counts_before = PreprocessBWT(bwt)
-  occurrence_counts = []
-  for pattern in patterns:
+    bwt = sys.stdin.readline().strip()
+    pattern_count = int(sys.stdin.readline().strip())
+    patterns = sys.stdin.readline().strip().split()
+    # Preprocess the BWT once to get starts and occ_count_before.
+    # For each pattern, we will then use these precomputed values and
+    # spend only O(|pattern|) to find all occurrences of the pattern
+    # in the text instead of O(|pattern| + |text|).
+    starts, occ_counts_before = PreprocessBWT(bwt)
+    occurrence_counts = []
+    for pattern in patterns:
     occurrence_counts.append(CountOccurrences(pattern, bwt, starts, occ_counts_before))
-  print(' '.join(map(str, occurrence_counts)))
+    print(' '.join(map(str, occurrence_counts)))
--- PA2/bwtinverse/bwtinverse.py
+++ PA2/bwtinverse/bwtinverse.py
@@ -1,11 +1,51 @@
 # python3
 import sys
 
-def InverseBWT(bwt):
-    # write your code here
-    return ""
 
+def InverseBWT(bwt):
+    bwt = list(bwt)
+    cs = countSort(bwt)
+    numb = [0] * 5
+    pointer = []
+    for i in range(len(bwt)):
+        x = select(bwt[i])
+        pointer.append(numb[x])
+        numb[x] += 1
+    x = 0
+    og = []
+    for i in range(len(bwt) - 1):
+        a = bwt[x]
+        n = select(a)
+        og.append(a)
+        x = cs[n] + pointer[x]
+    og.reverse()
+    return ''.join(og) + '$'
+
+
+def BWT(text):
+    arry = []
+    x = len(text)
+    for i in range(len(text)):
+        arry.append(text[i:] + text[0:i])
+    arry.sort()
+    out = ""
+    i = [z[-1] for z in arry]
+    return ''.join(i)
+
+
+def countSort(inp):
+    out = [0] * 5
+    for i in inp:
+        out[select(i)] += 1
+    out = [0] + out[:4]
+    for i in range(1,5):
+        out[i] += out[i-1]
+    return out
+
+def select(inp):
+    a = ['$','A','C','G','T']
+    return a.index(inp)
 
 if __name__ == '__main__':
-    bwt = sys.stdin.readline().strip()
-    print(InverseBWT(bwt))
+    inp = sys.stdin.readline().strip()
+    print(InverseBWT(inp))
