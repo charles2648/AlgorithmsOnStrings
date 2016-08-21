b0259beec6aac1684f82b30d888f3b5a47565c0a
androidwargames <adwagner89@gmail.com>
2016-08-21 12:02:58 -0500

    get prefix function test 1

--- PA3/kmp/kmp.py
+++ PA3/kmp/kmp.py
@@ -3,19 +3,29 @@ import sys
 
 
 def find_pattern(pattern, text):
-  """
-  Find all the occurrences of the pattern in the text
-  and return a list of all positions in the text
-  where the pattern starts in the text.
-  """
-  result = []
-  # Implement this function yourself
-  return result
+    """
+    Find all the occurrences of the pattern in the text
+    and return a list of all positions in the text
+    where the pattern starts in the text.
+    """
+    result = get_prefix(text)
+    return result
+
+
+def get_prefix(text):
+    out = [0] * len(text)
+    for i in range(1, len(text) - 1):
+        x = i - 1
+        while x > 0:
+            if text[i] == text[out[x]]:
+                out[i] = out[x] + 1
+            else:
+                x = out[x]
+    return out
 
 
 if __name__ == '__main__':
-  pattern = sys.stdin.readline().strip()
-  text = sys.stdin.readline().strip()
-  result = find_pattern(pattern, text)
-  print(" ".join(map(str, result)))
-
+    pattern = sys.stdin.readline().strip()
+    text = sys.stdin.readline().strip()
+    result = find_pattern(pattern, text)
+    print(" ".join(map(str, result)))
